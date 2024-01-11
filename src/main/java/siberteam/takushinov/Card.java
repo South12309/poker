f923898452f313f228da8f8c71e8dc10009f97c7
Ali Takushinov <south123@mail.ru>
2024-01-11 09:16:07 +0300

    Initial commit

---
+++ src/main/java/siberteam/takushinov/Card.java
@@ -0,0 +1,11 @@
+package siberteam.takushinov;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Card {
+    private char value;
+    private char suit;
+}
---
+++ src/main/java/siberteam/takushinov/PokerHand.java
@@ -0,0 +1,7 @@
+package siberteam.takushinov;
+
+import java.util.List;
+
+public class PokerHand {
+    private List<Card> cards;
+}
