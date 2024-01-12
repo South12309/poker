88bb22f0df20c06243b7e2d512867c4e9eb38f71
Ali Takushinov <south123@mail.ru>
2024-01-12 12:12:40 +0300

    Added tests that check the operation of compare combinations that did not pass before solving the previous problem (resorting the list taking into account the seniority of combinations)

--- src/test/java/siberteam/takushinov/PokerHandTest.java
+++ src/test/java/siberteam/takushinov/PokerHandTest.java
@@ -17,4 +17,22 @@ public class PokerHandTest {
         PokerHand pokerHand2 = new PokerHand("AS 5D 2S 2H TC");
         Assert.assertEquals(1,pokerHand1.compareTo(pokerHand2));
     }
+    @Test
+    public void compareSameCombinationSet() {
+        PokerHand pokerHand1 = new PokerHand("KS 9D 9S 2H 9C");
+        PokerHand pokerHand2 = new PokerHand("AS 5D 2S 2H 2C");
+        Assert.assertEquals(1,pokerHand1.compareTo(pokerHand2));
+    }
+    @Test
+    public void compareSameCombinationFullHouse() {
+        PokerHand pokerHand1 = new PokerHand("2S 9D 9S 2H 9C");
+        PokerHand pokerHand2 = new PokerHand("5S 5D 2S 2H 2C");
+        Assert.assertEquals(1,pokerHand1.compareTo(pokerHand2));
+    }
+    @Test
+    public void compareSameCombinationKare() {
+        PokerHand pokerHand1 = new PokerHand("2S 9D 9S 9H 9C");
+        PokerHand pokerHand2 = new PokerHand("5S 2D 2S 2H 2C");
+        Assert.assertEquals(1,pokerHand1.compareTo(pokerHand2));
+    }
 }
